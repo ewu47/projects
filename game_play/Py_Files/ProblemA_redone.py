0da675a762c0d22c33419e3a866bc321b3eaef12
ewu47 <emwu@uchicago.edu>
2025-02-14 16:43:51 -0600

    Delete game_play/Py_Files directory

--- game_play/Py_Files/ProblemA_redone.py
+++
@@ -1,7 +0,0 @@
-t = int(input())
-for _ in range(t):
-    x = input()
-    if int(x) % 33 == 0:
-        print("YES")
-    else:
-        print("NO")
--- game_play/Py_Files/sports_analytics/data_demo.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-prem_data_path = 'Premier League Player Stats.csv'
-prem_data = pd.read_csv(prem_data_path)
-print(prem_data.describe)
